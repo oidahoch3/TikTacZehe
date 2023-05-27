55d03b0aaa9966fe02eb4c03f2be5cb088351f92
oidahoch3 <walch.anna@hotmail.com>
2023-05-27 13:56:52 +0200

    learning variables

---
+++ main.py
@@ -0,0 +1,10 @@
+lex = 'yes'
+
+while lex == 'yes':
+    print('give kiss')
+    lex = input('is lex still cute? ')
+
+
+password = input('enter password: ')
+if password == 'cute':
+    print('still give kiss')
